b16d376e2cab76d635354630cbf26bd44382453a
Kristof Degros <96288164+syntrakristof@users.noreply.github.com>
2023-12-11 20:40:14 +0100

    Update stickerwinkel.sql

--- stickerwinkel.sql
+++ stickerwinkel.sql
@@ -1,29 +1,23 @@
 create or replace table colors
 (
-    id   smallint    not null
-        primary key,
-    name varchar(45) not null,
-    constraint name_UNIQUE
-        unique (name)
+    id   smallint    not null primary key,
+    color_name varchar(45) not null,
+    constraint name_UNIQUE unique (color_name)
 );
 
 create or replace table countries
 (
-    id   int auto_increment
-        primary key,
+    id   int auto_increment primary key,
     code varchar(3)  not null,
-    name varchar(50) not null,
-    constraint code_UNIQUE
-        unique (code),
-    constraint name_UNIQUE
-        unique (name)
+    country_name varchar(50) not null,
+    constraint code_UNIQUE unique (code),
+    constraint name_UNIQUE unique (country_name)
 );
 
 create or replace table customers
 (
-    id         int auto_increment
-        primary key,
-    name       varchar(200) not null,
+    id         int auto_increment primary key,
+    customer_name       varchar(200) not null,
     email      varchar(255) not null,
     street     varchar(100) null,
     street_nr  varchar(12)  null,
@@ -32,30 +26,25 @@ create or replace table customers
     country_id int          null,
     created    datetime     null,
     updated    datetime     null,
-    constraint email_UNIQUE
-        unique (email)
+    constraint email_UNIQUE unique (email)
 );
 
 create or replace definer = root@`127.0.0.1` trigger trg__customers_before_insert
-    before insert
-    on customers
-    for each row
+before insert on customers for each row
 BEGIN
- SET new.created = NOW();
+    SET new.created = NOW();
+    SET new.updated = NOW();
 END;
 
 create or replace definer = root@`127.0.0.1` trigger trg__customers_before_update
-    before update
-    on customers
-    for each row
+before update on customers for each row
 BEGIN
- SET new.updated = NOW();
+    SET new.updated = NOW();
 END;
 
 create or replace table sales
 (
-    id          int auto_increment
-        primary key,
+    id          int auto_increment primary key,
     customer_id varchar(45)    not null,
     street      varchar(12)    not null,
     street_nr   varchar(12)    not null,
@@ -67,70 +56,57 @@ create or replace table sales
 );
 
 create or replace definer = root@`127.0.0.1` trigger trg__sales_before_insert
-    before insert
-    on sales
-    for each row
+before insert on sales for each row
 BEGIN
- SET new.created = NOW();
+  SET new.created = NOW();
+  SET new.updated = NOW();
 END;
 
 create or replace definer = root@`127.0.0.1` trigger trg__sales_before_update
-    before update
-    on sales
-    for each row
+before update on sales for each row
 BEGIN
- SET new.updated = NOW();
+    SET new.updated = NOW();
 END;
 
 create or replace table sticker_types
 (
-    id   smallint    not null
-        primary key,
-    name varchar(45) not null,
-    constraint name_UNIQUE
-        unique (name)
+    id   smallint  not null primary key,
+    type_name varchar(45) not null,
+    constraint name_UNIQUE unique (type_name)
 );
 
 create or replace table stickers
 (
-    id             int auto_increment
-        primary key,
-    name           varchar(45)    not null,
+    id             int auto_increment primary key,
+    description    varchar(45)    not null,
     color_id       smallint       not null,
     stickertype_id smallint       not null,
     price          decimal(10, 2) not null,
     created        datetime       null,
     updated        datetime       null,
-    constraint fk_colortype_fk
-        foreign key (color_id) references colors (id),
-    constraint fk_stickertype_fk_fk
-        foreign key (stickertype_id) references sticker_types (id)
+    constraint fk_colortype_fk foreign key (color_id) references colors (id),
+    constraint fk_stickertype_fk_fk foreign key (stickertype_id) references sticker_types (id)
 );
 
 create or replace table sales_detail
 (
-    id         int auto_increment
-        primary key,
+    id         int auto_increment primary key,
     sales_id   int            not null,
     sticker_id int            not null,
     quantity   int            not null,
     price      decimal(10, 2) not null,
-    constraint FK_sales_detail_stickers
-        foreign key (sticker_id) references stickers (id)
+    constraint FK_sales_detail_stickers foreign key (sticker_id) references stickers (id)
 );
 
 create or replace definer = root@`127.0.0.1` trigger trg__stickers_before_insert
-    before insert
-    on stickers
-    for each row
+before insert on stickers for each row
 BEGIN
  SET new.created = NOW();
+ SET new.updated = NOW();
 END;
 
 create or replace definer = root@`127.0.0.1` trigger trg__stickers_before_update
-    before update
-    on stickers
-    for each row
+before update on stickers for each row
 BEGIN
  SET new.updated = NOW();
 END;
@@ -143,16 +119,15 @@ from (`stickerwinkel`.`sales` `s` join `stickerwinkel`.`sales_detail` `d` on (`d
 order by `s`.`created` desc;
 
 create or replace definer = root@`127.0.0.1` view view_sold_what_to_whom as
-select `s`.`price` AS `price`, `c`.`name` AS `name`, sum(`d`.`quantity`) AS `quantity`
+select `s`.`price` AS `price`, `c`.`customer_name` AS `name`, sum(`d`.`quantity`) AS `quantity`
 from ((`stickerwinkel`.`sales` `s` join `stickerwinkel`.`customers` `c`
        on (`c`.`id` = `s`.`customer_id`)) join `stickerwinkel`.`sales_detail` `d` on (`d`.`sales_id` = `s`.`id`))
 order by `s`.`price`;
 
 create or replace definer = root@`127.0.0.1` view view_top_5_products_sold as
-select `st`.`name` AS `productname`, sum(`d`.`quantity`) AS `quantity`, sum(`d`.`price`) AS `totalprice`
+select `st`.`description` AS `productname`, sum(`d`.`quantity`) AS `quantity`, sum(`d`.`price`) AS `totalprice`
 from ((`stickerwinkel`.`sales` `s` join `stickerwinkel`.`sales_detail` `d`
        on (`d`.`sales_id` = `s`.`id`)) join `stickerwinkel`.`stickers` `st` on (`d`.`sticker_id` = `st`.`id`))
 group by `st`.`name`
 order by sum(`d`.`quantity`) desc
 limit 5;
-
